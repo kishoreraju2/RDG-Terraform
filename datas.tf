e60e1c083aef44926ff67c217c8942fe8380df79
KIRAJU <KIRAJU@KIRAJU-IN.in.oracle.com>
2020-07-23 19:12:16 +0530

    show output

--- datas.tf
+++ datas.tf
@@ -2,3 +2,7 @@ data "oci_identity_availability_domain" "ad" {
   compartment_id = var.compartment_ocid
   ad_number      = 1
 }
+
+output "ad" {
+    value = oci_identity_availability_domain.ad.name
+}
